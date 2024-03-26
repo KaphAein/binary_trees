570d6393b35a6e807c8b7e02261e201329cdf4c2
kaphaein <cherifafadaly@gmail.com>
2024-03-26 16:12:17 -0700

    “Binary_Trees”

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node);
-	if (node->parent->right ==node)
-		return (node);
-	return (null);
+	return (node->parent->left);
 }
