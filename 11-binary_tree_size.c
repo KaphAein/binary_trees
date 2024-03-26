070d731bb99d8e1364c0b506f1dfa68f0b8ab357
kaphaein <cherifafadaly@gmail.com>
2024-03-26 14:18:50 -0700

    “Binary_Trees”

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -15,4 +15,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	left_size = tree->left ? 1 + binary_tree_size(tree->left) : 0;
 	right_size = tree->right ? 1 + binary_tree_size(tree->right) : 0;
 
-	return (left_size + right_size + 1)
+	return (left_size + right_size + 1);
+}
